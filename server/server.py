06a0313431296f92a78e2c70bd756db83daec3ba
Abhijeet Thombare <abhithombare45@gmail.com>
2025-07-11 23:20:25 +0530

    Fix: serve frontend files via Flask.

--- server/server.py
+++ server/server.py
@@ -1,27 +1,31 @@
-from flask import Flask, request, jsonify
-#import util
+from flask import Flask, request, jsonify, send_from_directory
 import server.util as util
-# from . import util
+import os
 
+# Serve files from '../client' directory
+app = Flask(__name__, static_folder="../client", static_url_path="")
 
-app = Flask(__name__)
-
+# Load model and columns
 util.load_saved_artifacts()
 
+# Serve home page
+@app.route('/')
+def serve_home():
+    return send_from_directory(app.static_folder, 'app.html')
 
-@app.route("/hello")
-def hello():
-    return "Hi, Abhijeet!"
+# Serve any static files (CSS, JS, etc.)
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
-
+# API endpoint for location list
 @app.route("/get_location_names")
 def get_location_names():
     response = jsonify({"locations": util.get_location_names()})
-    response.headers.add("Access-control-Allow-Origin", "*")
-
+    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
-
+# API endpoint for prediction
 @app.route("/predict_home_price", methods=["POST"])
 def predict_home_price():
     total_sqft = float(request.form["total_sqft"])
@@ -29,14 +33,8 @@ def predict_home_price():
     bhk = int(request.form["bhk"])
     bath = int(request.form["bath"])
 
-    response = jsonify(
-        {"estimated_price": util.get_estimated_price(location, total_sqft, bhk, bath)}
-    )
-
+    response = jsonify({
+        "estimated_price": util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
-
-
-if __name__ == "__main__":
-    print("Starting Python Flask File Server For Home Price Prediction...!")
-    app.run()
